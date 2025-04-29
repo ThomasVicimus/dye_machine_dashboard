e7588778501f122eb0f0740a92871ab6c24e0163
ThomasVicimus <ttsui@vicimus.com>
2025-04-29 00:29:32 -0400

    mobile layout

--- layouts/mobile_dashboard_layout.py
+++ layouts/mobile_dashboard_layout.py
@@ -1,43 +1,58 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc
+from PlotCharts.PlotChart_MachineUsage import create_chart1_layout
+from Database.serialize_df import serialize_dataframe_dict
+from layouts.create_buttons import create_period_button
 
 # Note: Figures are passed from mobile_app.py
 
 
-def create_main_dashboard_layout(initial_charts: dict, color_theme, lang):
-    """Creates the main mobile dashboard layout structure with clickable charts."""
+def create_mobile_layout(
+    initial_charts_data: dict,
+    color_theme,
+    lang,
+    default_period: str = "今日",
+):
+    """Creates the main mobile dashboard layout structure with clickable charts.
 
-    charts = {
-        "mobile-chart-1": initial_charts["machine_usage"],
-        "mobile-chart-2": initial_charts["machine_usage"],
-        "mobile-chart-3": initial_charts["machine_usage"],
-        "mobile-chart-4": initial_charts["machine_usage"],
-        "mobile-chart-5": initial_charts["machine_usage"],
-        "mobile-chart-6": initial_charts["machine_usage"],
-    }
+    Args:
+        initial_charts (dict): Dictionary mapping chart IDs to initial figure objects.
+        initial_chart_data (dict): Dictionary containing the initially fetched data for charts.
+        color_theme: The color theme setting.
+        lang: The language setting.
+    """
+    periods = initial_charts_data["chart1-data-store"].keys()
+    # charts = {
+    #     "mobile-chart-1": initial_charts_layout["machine_usage"],
+    #     "mobile-chart-2": initial_charts_layout["machine_usage"],
+    #     "mobile-chart-3": initial_charts_layout["machine_usage"],
+    #     "mobile-chart-4": initial_charts_layout["machine_usage"],
+    #     "mobile-chart-5": initial_charts_layout["machine_usage"],
+    #     "mobile-chart-6": initial_charts_layout["machine_usage"],
+    # }
 
-    def create_chart_column(chart_id, figure):
-        """Helper to create a Bootstrap column containing a clickable chart graph."""
-        graph_component = dcc.Graph(
-            id=chart_id,
-            figure=figure,
-            style={"height": "20vh", "width": "95%"},
-            config={"displayModeBar": False},
-        )
-        return dbc.Col(
-            dcc.Link(
-                graph_component,
-                href=f"/details/{chart_id}",  # Link using the chart ID
-                id=f"link-{chart_id}",
-                style={
-                    "display": "block",
-                    "height": "100%",
-                    "width": "100%",
-                },  # Ensure link covers graph
-            ),
-            width=4,
-            className="p-0",
-        )
+    # def create_chart_column(chart_id, figure):
+    #     """Helper to create a Bootstrap column containing a clickable chart graph."""
+    #     graph_component = dcc.Graph(
+    #         id=chart_id,
+    #         figure=figure,
+    #         style={"height": "20vh", "width": "95%"},
+    #         config={"displayModeBar": False},
+    #     )
+    #     return dbc.Col(
+    #         dcc.Link(
+    #             graph_component,
+    #             href=f"/details/{chart_id}",  # Link using the chart ID
+    #             id=f"link-{chart_id}",
+    #             style={
+    #                 "display": "block",
+    #                 "height": "100%",
+    #                 "width": "100%",
+    #             },  # Ensure link covers graph
+    #         ),
+    #         width=4,
+    #         className="p-0",
+    #     )
 
     return html.Div(
         id="mobile-wrapper",
@@ -61,30 +76,69 @@ def create_main_dashboard_layout(initial_charts: dict, color_theme, lang):
                             width=12,
                         )
                     ),
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                # Call the imported button creation function
+                                create_period_button(periods=periods),
+                                width=4,  # Align with first chart column
+                            ),
+                            dbc.Col(width=8),  # Empty columns to fill the row
+                        ],
+                        className="mb-2",
+                    ),
                     # Row 1 (Charts 1-3)
                     dbc.Row(
                         [
-                            create_chart_column(cid, fig)
-                            for cid, fig in list(charts.items())[:3]
+                            create_chart1_layout(
+                                default_period=default_period,
+                                dfs=initial_charts_data["chart1-data-store"],
+                                mobile=True,
+                                chart_id="chart-1",
+                            ),
+                            create_chart1_layout(
+                                default_period=default_period,
+                                dfs=initial_charts_data["chart1-data-store"],
+                                mobile=True,
+                                chart_id="chart-2",
+                            ),
+                            create_chart1_layout(
+                                default_period=default_period,
+                                dfs=initial_charts_data["chart1-data-store"],
+                                mobile=True,
+                                chart_id="chart-3",
+                            ),
                         ],
                         className="mb-1 g-0",
                         align="stretch",
                     ),
                     # Row 2 (Charts 4-6)
-                    dbc.Row(
-                        [
-                            create_chart_column(cid, fig)
-                            for cid, fig in list(charts.items())[3:]
-                        ],
-                        className="mb-1 g-0",
-                        align="stretch",
-                    ),
+                    # dbc.Row(
+                    #     [
+                    #         create_chart_column(cid, fig)
+                    #         for cid, fig in list(charts.items())[3:]
+                    #     ],
+                    #     className="mb-1 g-0",
+                    #     align="stretch",
+                    # ),
                     # Placeholder for potential future updates or controls
                     html.Div(
                         id="mobile-dynamic-content", className="text-white text-center"
                     ),
                     dcc.Interval(
                         id="mobile-interval", interval=60 * 1000, n_intervals=0
+                    ),
+                    # TODOL Json serialize the data store
+                    # Add the data store here and populate with initial data
+                    dcc.Store(
+                        id="chart1-data-store",
+                        data=serialize_dataframe_dict(
+                            initial_charts_data["chart1-data-store"]
+                        ),
+                    ),
+                    dcc.Store(
+                        id="time-period-store",
+                        data=default_period,
                     ),
                 ],
                 fluid=True,
