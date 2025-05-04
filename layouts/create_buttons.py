1ac582e8d136bb40b3af5a218fb1fc533f75ced1
ThomasVicimus <ttsui@vicimus.com>
2025-05-04 16:41:10 -0400

    create color button

--- layouts/create_buttons.py
+++ layouts/create_buttons.py
@@ -20,4 +20,31 @@ def create_period_button(periods):
     )
 
 
+def create_theme_buttons():
+    """Creates colored buttons for theme selection."""
+    colors = {
+        # "white": {"bg": "#fdfefe", "border": "#3c3c3c"},
+        "black": {"bg": "#202020", "border": "#3c3c3c"},
+        "dark_blue": {"bg": "#1e3d59", "border": "#3c3c3c"},
+    }
+
+    return dbc.ButtonGroup(
+        [
+            dbc.Button(
+                "",
+                id={"type": "theme-button", "index": color_name},
+                style={
+                    "background-color": color_data["bg"],
+                    "border-color": color_data["border"],
+                    "height": "30px",
+                    "width": "30px",
+                },
+                size="sm",
+            )
+            for color_name, color_data in colors.items()
+        ],
+        className="mb-2",
+    )
+
+
 # *TODO Color Theme button
