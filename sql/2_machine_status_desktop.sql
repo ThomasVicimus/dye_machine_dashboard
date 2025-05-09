85e395f7cc4f2903bda612dd43318da46508206b
ThomasVicimus <ttsui@vicimus.com>
2025-05-09 04:47:45 -0400

    desktop & mobile sql

---
+++ sql/2_machine_status_desktop.sql
@@ -0,0 +1,13 @@
+select 
+    machine_name,
+    state,
+    user_prompt,
+    num_of_alarms,
+    mt_temperature,
+    batch_no,
+    program_name,
+    current_step,
+    next_step,
+    minutes_run,
+    expected_finish_time
+from machine_status
