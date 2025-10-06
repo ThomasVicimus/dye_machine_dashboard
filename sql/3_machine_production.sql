d77a7abc4caf6447d84a41b197a26aba319949f2
ThomasVicimus <ttsui@vicimus.com>
2025-10-06 15:37:15 -0400

    Squashed new chart3 tb;

--- sql/3_machine_production.sql
+++ sql/3_machine_production.sql
@@ -1,8 +1,10 @@
 select 
     machine_name,
     date,
-    weight_kg
-from machine_production_waste
-where order_index = 1
+    weight_kg,
+    order_index
+from production_volume_log
+where period in ({period_replace})
+-- where order_index = 1
 -- group by date
-order by date desc, machine_name;
+order by date, machine_name;
